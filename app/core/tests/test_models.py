c30f922001cc6c6d5ca4a8f82cf18c2970556a2e
3mlifamily <leeworld88@yahoo.com>
2020-05-24 19:59:05 -0500

    add custom model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -25,6 +25,15 @@ class ModelTests(TestCase):
         self.assertEqual(user.email, email.lower())
 
     def test_new_user_invalid_email(self):
-        ""Test creating user with no email reaises error"""
+        """Test creating user with no email reaises error"""
         with self.assertRaise(ValueError):
             get_user_model.objects.create_user(None, 'test1234')
+
+    def test_create_new_superuser(self):
+        ""Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'mlisuper@gmail.com',
+            'test1224'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
